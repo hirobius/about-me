2fd79129906874285fadee28e7c171fc06fe704d
Adrian Milsap <adrian@hirobius.com>
2021-01-13 20:04:28 -0800

    first pass at questions

--- js/app.js
+++ js/app.js
@@ -1,22 +1,35 @@
 'use strict';
-
 console.log('Howdy!');
 
-var hello = 42;
+// Ask the User their name through a prompt
+// Display that name back to the user through a custom greeting welcoming them to your site.
 
-console.log(hello);
+var username = prompt('Hello! What\'s your name?');
+alert('Lorem 0 ' + username + ' Ipsum 0');
 
-// get username and offer greeting
 
-var username = prompt('Hi what\'s your name?');
-alert('Hello ' + username + ' Nice to meet ya!');
+// Prompt the user a total of exactly five yes or no questions.
+// The user input for the answer must accept either y/n or yes/no responses while taking into consideration case sensitivity
 
-var questionTwo = prompt('Hi what\'s your name?');
-alert('Hello ' + questionTwo + ' Nice to meet ya!');
+// var questionOne = prompt('Question 1?').toUpperCase;
+// alert('Lorem 1 ' + questionOne + ' Ipsum 1');
 
-var questionOne = prompt('When\'s your birthday?').toLocaleLowerCase();
+// var questionTwo = prompt('Question 2?').toUpperCase;
+// alert('Lorem 2 ' + questionTwo + ' Ipsum 2');
 
-if(questionOne === 'yes' || questionOne === 'y') {
-  // console.log('you are correct');
-  alert('Nice.');
-}
+// var questionThree = prompt('Question 3?').toUpperCase;
+// alert('Lorem 3 ' + questionThree + ' Ipsum 3');
+
+// var questionFour = prompt('Question 4?').toUpperCase;
+// alert('Lorem 4 ' + questionFour + ' Ipsum 4');
+
+// var questionFive = prompt('Question 5?').toUpperCase;
+// alert('Lorem 4 ' + questionFive + ' Ipsum 4');
+
+
+// if(questionOne === 'yes' || questionOne === 'y') {
+//   // console.log('you are correct');
+//   alert('Nice.');
+// }
+
+// Display the user’s name back to them in your final message to the user.
