80433d4fb712797db4b42c680b49b272857cdd14
Adrian Milsap <adrian@hirobius.com>
2021-01-18 22:38:03 -0800

    working question 6

--- js/app.js
+++ js/app.js
@@ -99,55 +99,99 @@ if (quizFive === 'YES' || quizFive === 'Y') {
 // There should be exactly 4 guesses allowed
 // After all eamples have been exhausted, we must alert the user the correct answer, (Consider using an loop of some sort)
 
-for (var i = 4; i > 1; i--) {
-  var guessFingers = +prompt('How many fingers am I holding up?' + i + 'guesses remaining...');
+
+
+
+
+
+for (var i = 0; i < 4; i++) {
   var fingers = 7;
+  var fingerQuestion = 'How many fingers am I holding up?';
+  var guessFingers = prompt(fingerQuestion + ' You have 4 guesses total.');
   if (guessFingers === fingers) {
     alert('Excellent.');
     score++;
     break;
+  } else if (i === 2) {
+    alert('Last guess.');
   } else if (i < 3 && guessFingers < fingers) {
     alert('Higher...');
   } else if (i < 3 && guessFingers > fingers) {
     alert('Lower...');
-  } else if (i === 3) {
-    alert('Last guess.');
   }
 }
 
-
-
-// The last question, #7 will have multiple correct answers possible that are stored in an array
-// There will be 6 guesses allowed
-// The guesses will end once the user guesses a correct answer or they run out of attempts.
-// Display all the possible correct answers to the user.
-// Consider using a loop of some sort for this question.
-// At the very end, the user should be shown how many answers they got right out of 7. Keep track of them the whole time
-
-var answerCorrectly = false;
-var numberOfGuesses = 6;
-var myFavoriteAnimals = ['Lorem 1', 'Lorem 2', 'Lorem 3', 'Lorem 4'];
-//console.log(myFavoriteAnimals);
-for (var j = 0; j < numberOfGuesses; j++) {
-  var guessMyFavorites = prompt('What are my Favorite animals you have ${numberOfGuesses - j} guesses remaining').toUpperCase();
-  for (var k = 0; k < myFavoriteAnimals.length; k++) {
-    if (guessMyFavorites === myFavoriteAnimals[k]) {
-      alert('Good Job!');
-      answerCorrectly = true;
-      score++;
-      break;
-    }
-
-  }
-  if (answerCorrectly) {
-    break;
-  }
-}
+prompt('Your final score is: ' + score + '.');
 
 
 
 
-//     // while loop conversion
+
+
+// for (var i = 0; i < 4; i++) {
+//   var guessCounter = 4;
+//   var fingers = 7;
+//   var fingerQuestion = 'How many fingers am I holding up?';
+//   var guessFingers = prompt(fingerQuestion + guessCounter + ' guesses remaining.');
+//   if (fingers === guessFingers) {
+//     score++;
+//     alert('Excellent.');
+//     break;
+//   } else if (i < 4 && guessFingers < fingers) {
+//     alert('Higher...');
+//     guessCounter--;
+//   } else if (i < 4 && guessFingers > fingers) {
+//     alert('Lower...');
+//     guessCounter--;
+//   } else if (i === 1) {
+//     alert('Last guess.');
+//     guessCounter--;
+//   }
+// }
+
+
+
+
+
+
+
+// // The last question, #7 will have multiple correct answers possible that are stored in an array
+// // There will be 6 guesses allowed
+// // The guesses will end once the user guesses a correct answer or they run out of attempts.
+// // Display all the possible correct answers to the user.
+// // Consider using a loop of some sort for this question.
+// // At the very end, the user should be shown how many answers they got right out of 7. Keep track of them the whole time
+
+// var answerCorrectly = false;
+// var numberOfGuesses = 6;
+// var myFavoriteAnimals = ['Lorem 1', 'Lorem 2', 'Lorem 3', 'Lorem 4'];
+// //console.log(myFavoriteAnimals);
+// for (var j = 0; j < numberOfGuesses; j++) {
+//   var guessMyFavorites = prompt('What are my Favorite animals you have ${numberOfGuesses - j} guesses remaining').toUpperCase();
+//   for (var k = 0; k < myFavoriteAnimals.length; k++) {
+//     if (guessMyFavorites === myFavoriteAnimals[k]) {
+//       alert('Good Job!');
+//       answerCorrectly = true;
+//       score++;
+//       break;
+//     }
+
+//   }
+//   if (answerCorrectly) {
+//     break;
+//   }
+// }
+
+
+
+
+
+
+
+//     // while loop conversion solution
+
+
+
 //     var answerCorrectly = false;
 //     var numberOfGuesses = 6;
 //     var myFavoriteAnimals = ['Lorem 1', 'Lorem 2', 'Lorem 3', 'Lorem 4'];
